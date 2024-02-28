87f8f99dbaca588fce358ccc01b11f874566a5cb
ALUNO <ALUNO@LABFPC22>
2024-02-28 11:26:15 -0300

    aula parte 1

--- aula3/Program.cs
+++ aula3/Program.cs
@@ -10,6 +10,20 @@ namespace aula3
     {
         static void Main(string[] args)
         {
+            int a = 1234567891; // 10^9
+            uint b = 1234567899; // 10^10
+            long c = 1234567891011121314; // 10^19
+            ulong d = 12345678910111213141; // 10^20
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+
+            float f = 1.27F; // especificar tipo no final
+            double x = 1.23; // especificar tipo no final
+            decimal = 1.42m; // especificar tipo no final - 28 digitos de precisão.
+
         }
     }
 }
